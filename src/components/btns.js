9e95942afd8d27bd6e9cc28f69573a55a7b48fb9
simskiy <simskiy@gmail.com>
2021-02-21 10:52:02 +1000

    Подключил библиотеку mathjs (чтобы не изобретать велосипед)

---
+++ src/components/btns.js
@@ -0,0 +1,114 @@
+const buttons = [
+  [
+    {
+      title: '(',
+      type: 'operator',
+      value: '('
+    },
+    {
+      title: ')',
+      type: 'operator',
+      value: ')'
+    },
+    {
+      title: '%',
+      type: 'operator',
+      value: ''
+    },
+    {
+      title: 'CE',
+      type: 'cancel',
+      value: ''
+    }
+  ],
+  [
+    {
+      title: '7',
+      type: 'number',
+      value: '7'
+    },
+    {
+      title: '8',
+      type: 'number',
+      value: '8'
+    },
+    {
+      title: '9',
+      type: 'number',
+      value: '9'
+    },
+    {
+      title: '&divide;',
+      type: 'operator',
+      value: '/'
+    }
+  ],
+  [
+    {
+      title: '4',
+      type: 'number',
+      value: '4'
+    },
+    {
+      title: '5',
+      type: 'number',
+      value: '5'
+    },
+    {
+      title: '6',
+      type: 'number',
+      value: '6'
+    },
+    {
+      title: '&times;',
+      type: 'operator',
+      value: '*'
+    }
+  ],
+  [
+    {
+      title: '1',
+      type: 'number',
+      value: '1'
+    },
+    {
+      title: '2',
+      type: 'number',
+      value: '2'
+    },
+    {
+      title: '3',
+      type: 'number',
+      value: '3'
+    },
+    {
+      title: '-',
+      type: 'operator',
+      value: '-'
+    }
+  ],
+  [
+    {
+      title: '0',
+      type: 'number',
+      value: '0'
+    },
+    {
+      title: '.',
+      type: 'operator',
+      value: '.'
+    },
+    {
+      title: '=',
+      type: 'enter',
+      value: ''
+    },
+    {
+      title: '+',
+      type: 'operator',
+      value: '+'
+    }
+  ]
+]
+
+export default buttons
